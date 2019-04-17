7039ffbe0343d8ea473329e0c2bb5feb8dfcd45d
LizAitken <49604170+LizAitken@users.noreply.github.com>
2019-04-17 07:05:12 -0400

    Add files via upload
    "First commit"

---
+++ index.py
@@ -0,0 +1,5 @@
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+fav_color = input("What is your favorite color? ")
+
+print("Hello %s %s! My favorite color is also %s!" %(first_name, last_name, fav_color))
